57443a6f92c425a336582486faccfce7a523bc29
Janne Koschinski <janne@kuschku.de>
2018-03-18 23:14:56 +0100

    Attempt at autosave

--- assets/js/page_image_detail.js
+++ assets/js/page_image_detail.js
@@ -1,21 +1,53 @@
+function postData(url, data) {
+    return fetch(url, {
+        body: data,
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        method: 'POST',
+        mode: 'cors',
+        redirect: 'follow'
+    }).then(response => response.json())
+}
+
 const fakeTitle = document.querySelector(".title.fake-input[contenteditable]");
 const fakeDescription = document.querySelector(".description.fake-input[contenteditable]");
 
 const actualTitle = document.querySelector(".update-form input[name=title]");
 const actualDescription = document.querySelector(".update-form input[name=description]");
 
+const save = document.querySelector("#save");
+const updateForm = document.querySelector(".update-form");
+
+let lastTimeOut = null;
+
+const doSave = () => {
+    const data = new FormData(document.forms.namedItem("upload"));
+    save.value = "Saving…";
+    postData(location.href, data).then((json) => {
+        save.value = "Saved";
+    })
+};
+
+const scheduleSave = () => {
+    if (lastTimeOut !== null) {
+        clearTimeout(lastTimeOut);
+    }
+    lastTimeOut = setTimeout(doSave, 300)
+};
+
 const fakeTitleListener = (event) => {
     requestAnimationFrame(() => {
         document.title = event.target.innerText + " | i.k8r";
         actualTitle.value = fakeTitle.innerText;
-    })
+    });
+    scheduleSave();
 
 };
 const fakeDescriptionListener = (event) => {
     requestAnimationFrame(() => {
         actualDescription.value = fakeDescription.innerText;
-    })
-
+    });
+    scheduleSave();
 };
 
 // Insert <br> between lines instead of \n for editing
@@ -33,3 +65,9 @@ fakeTitle.addEventListener("keypress", fakeTitleListener);
 
 fakeDescription.addEventListener("input", fakeDescriptionListener);
 fakeDescription.addEventListener("keypress", fakeDescriptionListener);
+
+save.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    doSave();
+});
