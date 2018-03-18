862d309e49a7fa13da9d463d095a4fd550bdd76f
Janne Koschinski <janne@kuschku.de>
2018-03-18 02:16:06 +0100

    Minor UI improvements

--- assets/js/fake-input.js
+++ assets/js/fake-input.js
@@ -5,6 +5,13 @@ const keyListener = (event) => {
     }
 };
 
+const changeListener = (event) => {
+    requestAnimationFrame(() => {
+        event.target.innerText = event.target.innerText
+    })
+};
+
 Array.prototype.slice.call(document.querySelectorAll(".fake-input[contenteditable]")).forEach(elem => {
-    elem.addEventListener("keypress", keyListener)
+    elem.addEventListener("keypress", keyListener);
+    elem.addEventListener("change", changeListener);
 });
