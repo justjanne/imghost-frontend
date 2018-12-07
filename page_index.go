67329809a7e5a861f6a5bca18473a6ef30091008
Janne Koschinski <janne@kuschku.de>
2018-12-07 16:43:28 +0100

    show image list by default

--- page_index.go
+++ page_index.go
@@ -21,13 +21,7 @@ func removeFileExtensions(path string) string {
 func pageIndex(ctx PageContext) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
-			user := parseUser(r)
-
-			if err := formatTemplate(w, "index.html", IndexData{
-				user,
-			}); err != nil {
-				panic(err)
-			}
+			http.Redirect(w, r, "/me/images", http.StatusFound)
         } else if r.URL.Path == "/favicon.ico" {
 			w.Header().Set("Vary", "Accept-Encoding")
 			w.Header().Set("Cache-Control", "public, max-age=31536000")
