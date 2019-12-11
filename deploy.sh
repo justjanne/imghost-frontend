129593561fb8a008e9d12cdb513a4141fea430f2
Janne Koschinski <janne@kuschku.de>
2019-12-11 19:39:48 +0100

    Updated build scripts

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-IMAGE=k8r.eu/justjanne/imghost-frontend
-TAGS=$(git describe --always --tags HEAD)
-DEPLOYMENT=imghost-frontend
-POD=imghost-frontend
-
-kubectl -n imghost set image deployment/$DEPLOYMENT $POD=$IMAGE:$TAGS
